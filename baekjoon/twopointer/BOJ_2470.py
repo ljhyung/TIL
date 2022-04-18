8218f4f5a72fa59b896d0109846badb8ef5a95fe
ljhyung <ljjuni1114@gmail.com>
2022-04-19 00:04:01 +0900

    BOJ_2470 clear

--- baekjoon/twopointer/BOJ_2470.py
+++ baekjoon/twopointer/BOJ_2470.py
@@ -1,19 +1,24 @@
-def solve():
-    minnum = abs(lst[0]+lst[N-1])
-    st=0
-    en=N-1
-    for i in range(N):
-        for j in range(N - 1, i, -1):
-            a = abs(lst[i] + lst[j])
-            if a == 0:
-                return lst[i], lst[j]
-            elif a<minnum:
-                minnum = a
-                st = i
-                en = j
-    return lst[st], lst[en]
-
 N = int(input())
 lst = list(map(int, input().split()))
 lst.sort()
-print(solve())
+st = 0
+en = N-1
+resst = 0
+resen = N-1
+minnum = abs(lst[st]+lst[en])
+while st<en:
+    a = lst[st]+lst[en]
+    if a==0:
+        resst = st
+        resen = en
+        break
+    if abs(a)<minnum:
+        minnum = abs(a)
+        resst = st
+        resen = en
+    if a<0:
+        st += 1
+    elif a>0:
+        en -= 1
+
+print(lst[resst], lst[resen])
