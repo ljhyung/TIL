68e9ebe236c01bc1b150927affd41b9279a1113c
ljhyung <ljjuni1114@gmail.com>
2022-06-05 21:53:48 +0900

    add algo

---
+++ baekjoon/etc/BOJ_5639.py
@@ -0,0 +1,17 @@
+'''
+BOJ
+이진 검색 트리
+골드 5
+
+https://www.acmicpc.net/problem/5639
+'''
+
+nums = []
+while True:
+    try:
+        temp = int(input())
+        nums.append(temp)
+    except:
+        break
+
+
