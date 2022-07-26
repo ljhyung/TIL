a4bd6ceeedee89dacb2752dc38151d78b2e410c0
LJH <ljjuni1114@gmail.com>
2022-07-26 23:38:54 +0900

    modi kakao algo

--- kakao_algo/2020_blind/PGS_외벽 점검.py
+++ kakao_algo/2020_blind/PGS_외벽 점검.py
@@ -8,6 +8,22 @@ https://school.programmers.co.kr/learn/courses/30/lessons/60062
 
 def solution(n, weak, dist):
     answer = 0
+    visited = [0 for _ in range(n)]
+    def sol(d):
+        if d==len(dist):
+            if weak:
+                answer=-1
+                return
+            else:
+                return d
+        if not weak:
+            return d
+        for i in range(len(weak)):
+            if visited[i]==0:
+                for j in range(len(weak)):
+
+        return
+    sol(0)
     return answer
 
 
